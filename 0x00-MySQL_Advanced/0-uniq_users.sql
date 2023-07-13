8153d2ad681f8f3176e1f0ecf499e51c58262ed8
JI-Maina <irungujmaina@gmail.com>
2023-07-13 08:24:39 +0000

    added Readme at the base of directory

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
