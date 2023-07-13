114d3d7cf259e5ed66557c5a31e03beefaf58cdf
JI-Maina <irungujmaina@gmail.com>
2023-07-13 07:54:03 +0000

    initial commit

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- SQL script that creates a table users with:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NUL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+);
